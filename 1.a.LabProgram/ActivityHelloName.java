68a3a9d499f8c50bb72fc611b193b24323fd4d40
VaishnaviKajjapu <51435303+VaishnaviKajjapu@users.noreply.github.com>
2021-01-12 17:22:10 +0530

    Add files via upload

---
+++ 1.a.LabProgram/ActivityHelloName.java
@@ -0,0 +1,11 @@
+package com.example.androidlab;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityHelloName extends AppCompatActivity {
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activitity_hello_name);
+        }
+    }
